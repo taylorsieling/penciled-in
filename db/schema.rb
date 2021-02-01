e570dd3de07baabc049db5194c9458aab804ffb6
taylorsieling <taylorasieling@gmail.com>
2021-02-01 15:46:09 -0600

    added location attr to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_181201) do
+ActiveRecord::Schema.define(version: 2021_02_01_214509) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_02_01_181201) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "location"
   end
 
   create_table "rsvps", force: :cascade do |t|
