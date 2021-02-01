89f2f9449c6d3965e54260b334a9931455ec944e
taylorsieling <taylorasieling@gmail.com>
2021-02-01 12:00:56 -0600

    finalizing omniauth login

--- Gemfile
+++ Gemfile
@@ -31,10 +31,10 @@ gem 'bootsnap', '>= 1.4.4', require: false
 gem 'bcrypt'
 
 gem 'omniauth'
-
 gem 'omniauth-google-oauth2'
+gem 'omniauth-rails_csrf_protection'
 
-gem 'dotenv'
+gem 'dotenv-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
     has_secure_password
+
+    def self.from_omniauth(response)
+        User.find_or_create_by(uid: response[:uid], provider: response["provider"]) do |u|
+            u.username = response[:info][:name]
+            u.email = response[:info][:email]
+            u.password = SecureRandom.hex(15)
+        end
+    end
+
 end
