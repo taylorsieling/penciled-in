a58bb5674a0d10ed45b009596c2151198fcabc11
taylorsieling <taylorasieling@gmail.com>
2021-02-01 15:44:47 -0600

    built event_params method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,31 @@
 class EventsController < ApplicationController
 
+    before_action :set_event, only: [:show]
+
+    def index
+        @events = Event.ordered_by_date
+    end
+
+    def show
+    end
+
     def new
         @event = Event.new
+    end
+
+    def create
+        byebug
+
+    end
+
+    private
+
+    def set_event
+        @event = Event.find(params[:id])
+    end
+
+    def event_params
+        params.require(:event).permit(:name, :description, :start_date, :end_date, :start_time, :end_time)
     end
 
 
