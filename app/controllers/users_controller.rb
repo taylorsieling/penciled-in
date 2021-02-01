a9d07dc546dd46976feb96627efa4b07e4a8bd76
taylorsieling <taylorasieling@gmail.com>
2021-02-01 12:59:14 -0600

    def user methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,37 @@
 class UsersController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create 
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def current_user
+    end
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :password, :email)
+    end
    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
     has_many :rsvps
     has_many :event_rsvps, through: :rsvps, source: :event
 
+    validates :password, presence: true
+
     def self.from_omniauth(response)
         User.find_or_create_by(uid: response[:uid], provider: response["provider"]) do |u|
             u.username = response[:info][:name]
