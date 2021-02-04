aaebea998cb366a35cb7908a186ef02b628bcd36
taylorsieling <taylorasieling@gmail.com>
2021-02-04 15:09:26 -0600

    added selection box to event form

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
     has_many :events
-    validates :name, presence: true
+    validates :name, presence: true, uniqueness: { case_sensitive: false }
     accepts_nested_attributes_for :events
 end
